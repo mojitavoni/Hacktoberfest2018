a3db9ea073d149cee4b0933f6f6b088b32bc617c
mojitavoni <55715328+mojitavoni@users.noreply.github.com>
2019-10-30 11:18:35 +0100

    Create pjs.js

---
+++ Prime-Number/pjs.js
@@ -0,0 +1,10 @@
+function checkPrime(n) {
+  isPrime = true
+  for (i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+        }
+    }
+  return isPrime
+}
